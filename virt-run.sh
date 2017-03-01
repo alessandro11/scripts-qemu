c097a240efffcaaf43daf4f894fa97cf537839ab
Alessandro Elias <ae11@c3sl.ufpr.br>
2017-03-01 20:09:15 -0300

    Made workaroud for -append parameter.
    Signed-off-by: Alessandro Elias <ae11@c3sl.ufpr.br>

--- virt-run.sh
+++ virt-run.sh
@@ -2,8 +2,10 @@
 
 
 PREFIX_SPICE_SOCKET_DIR=/tmp
-CONFIG_DIR=$HOME/vms/configs
+CONFIG_DIR=./configs
 
+# TODO:
+#		make help with all available options (default variables).
 if [ $# -lt 1 ]; then
     echo "Wrong number of parameters, pass one config from '$CONFIG_DIR'."
 	echo "export OPEN_VNC="
@@ -23,6 +25,8 @@ source $CONFIG_DIR/$1
 VM_DISK_IF=${VM_DISK_IF:-virtio}
 VM_NIC_MODEL=${VM_NIC_MODEL:-virtio}
 
+VM_CORES=${VM_CORES:-1}
+
 #
 # Extra parameters to qemu
 #
@@ -30,13 +34,20 @@ EXTRA_PARAM=${EXTRA_PARAM:-""}
 # Parse if has been declared a kernel.
 if [ -n "$VM_KERNEL" ]; then
     EXTRA_PARAM="$EXTRA_PARAM -kernel $VM_KERNEL"
-    if [ -n "$VM_KERNEL_CMDLINE" ]; then
-        EXTRA_PARAM="$EXTRA_PARAM -append $VM_KERNEL_CMDLINE"
-    fi
-fi
-# Parse if has been declared a kernel.
-if [ -n "$VM_INITRD" ]; then
-    EXTRA_PARAM="$EXTRA_PARAM -initrd $VM_INITRD"
+
+	# Parse if has been declared a kernel.
+	if [ -n "$VM_INITRD" ]; then
+		EXTRA_PARAM="$EXTRA_PARAM -initrd $VM_INITRD"
+	fi
+
+	# Still it is not a elegant way do solve the problem
+	# the problems is VM_KERNEL_CMDLINE is expanding twice
+	# once in read QEMU_PARAM and another in running qemu,
+	# so when there are two or parameters they are break
+	# as two or more parameters.
+    #if [ -n "$VM_KERNEL_CMDLINE" ]; then
+    #    EXTRA_PARAM="$EXTRA_PARAM -append \$VM_KERNEL_CMDLINE"
+    #fi
 fi
 
 # qemu executable
@@ -45,10 +56,10 @@ QEMU_PID=""
 
 help() {
     echo -e $"\nUsage: `basename $0`\n \
-        \tp|P PID QEMU,VNC
+        \tp|P PID QEMU,VNC \
         \tq|q exit\n \
         \tr|R reboot\n \
-        \tv|V vnc [vnc display]\n\n"
+        \tv|V vnc [display]\n\n"
 }
 
 run_virt() {
@@ -57,16 +68,24 @@ run_virt() {
 	# so we can copy and paste qemu command line.
 	echo $QEMU_PARAM
 
-    $QEMU_BIN $QEMU_PARAM &
+	# this if is a workaround that is agly.
+	if [ -z "$VM_KERNEL_CMDLINE" ]; then
+		$QEMU_BIN $QEMU_PARAM &
+	else
+		$QEMU_BIN $QEMU_PARAM -append "'$VM_KERNEL_CMDLINE'" &
+	fi
     QEMU_PID=$!
     
 	sleep 1
     ip link set dev "$VM_GUEST_IFNAME1" up || exit 1
 	ip link set "$VM_GUEST_IFNAME1" master "$HOST_BRD" || exit 1
     
-	[[ "${OPEN_VNC}x" != "x" ]] && gvncviewer "localhost:$OPEN_VNC" &> /dev/null&
+	if [ "${OPEN_VNC}x" == "spicex" ]; then
+		spicy --uri="spice+unix:///${PREFIX_SPICE_SOCKET_DIR}/${SPICE_SOCKET}"
+	elif [ "${OPEN_VNC}x" != "x" ]; then
+	   gvncviewer "localhost:$OPEN_VNC" &> /dev/null&
+	fi
 }
-
 
 finish() {
     [[ -n "$VNC_PID" ]] && kill "$VNC_PID"
@@ -84,7 +103,7 @@ case "$VM_BOOT" in
 			-balloon virtio
 			-enable-kvm
 			-cpu host
-			-smp cores=2,threads=$VM_SMP
+			-smp cores=$VM_CORES,threads=$VM_SMP
 			-rtc base=localtime,clock=host
 			-boot c
 			-drive file=$VM_DISK,index=0,if=$VM_DISK_IF,format=$VM_DISK_FMT,cache=writeback,cache.direct=on,aio=native
@@ -157,17 +176,19 @@ EOF
 	;;
 
 	*)
+		SPICE_SOCKET="vm_spice_$VM_NAME.socket"
 		read -d '' QEMU_PARAM <<EOF
 			-name $VM_NAME
-			-m $VM_RAM
-			-cpu host
-			-smp $VM_SMP
 			-enable-kvm
-			-localtime
-			-drive file=$VM_DISK,if=$VM_DISK_IF,format=$VM_DISK_FMT,cache=writeback
+			-cpu host
+			-smp cores=$VM_CORES,threads=$VM_SMP
+			-m $VM_RAM
+			-rtc base=localtime,clock=host
+			-drive file=$VM_DISK,if=$VM_DISK_IF,format=$VM_DISK_FMT,cache=writeback,cache.direct=on,aio=native
+			-spice unix,addr=${PREFIX_SPICE_SOCKET_DIR}/${SPICE_SOCKET},disable-ticketing
 			-net nic,macaddr=$VM_MAC1,vlan=$VM_VLAN1,model=$VM_NIC_MODEL
 			-net tap,vlan=$VM_VLAN1,ifname=$VM_GUEST_IFNAME1,script=no,downscript=no
-			-vga std
+			-vga cirrus
 			-vnc :$VNC_DISPLAY
 			$EXTRA_PARAM
 EOF
@@ -191,7 +212,7 @@ while :; do
             ;;
         'r'|'R')
             [[ -n "$QEMU_PID" ]] && kill "$QEMU_PID"
-            run_virt
+			run_virt
             ;;
         'q'|'Q')
 			finish
@@ -213,4 +234,3 @@ while :; do
     esac
 done
 
-unset PREFIX_SPICE_SOCKET_DIR
